58cf3c0a0ff2f37e77635b1b12b1e40019fec5da
Azuko <90575042+AzukoTazumaki@users.noreply.github.com>
2023-05-03 20:26:28 +0300

    Delete draft.py

--- draft.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-result_list = []
-num = int("-1")
-# for arr in np.array_split(, 3):
-#     result_list.append(list(arr))
-# average = round(float(np.mean(result_list[0] + result_list[1])), 3)
-# print(average)
-print(num)
